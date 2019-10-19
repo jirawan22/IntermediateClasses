9d0c3604496721946fed9bfdda17d644b8b5c224
jirawan22 <56579285+jirawan22@users.noreply.github.com>
2019-10-19 23:03:49 +0700

    2_6_UpdatePerson

--- 2.Classes/2_6AccessModifiers/Program.cs
+++ 2.Classes/2_6AccessModifiers/Program.cs
@@ -8,12 +8,23 @@ namespace AccessModifiers
 {
     public class Person
     {
+        private DateTime _birthdate;
 
-    }
+        public void SetBirthdate(DateTime birthdate)
+        {
+            _birthdate = birthdate;
+        }
+
+        public DateTime GetBirthdate()
+        {
+            return _birthdate;
+        }
+}
     class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
